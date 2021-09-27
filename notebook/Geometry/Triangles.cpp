d32d96b8eb7ba5a231caca52fa1f48c79d1b22fd
Hamidreza <hamidreza004@gmail.com>
2021-09-27 15:36:06 +0330

    Add triangles

---
+++ notebook/Geometry/Triangles.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long double ld;
+typedef complex<ld> pt;
+typedef vector<pt> poly;
+#define x real()
+#define y imag()
+
+typedef pair<pt, pt> line;
+// +, -, * scalar well defined
+const ld EPS = 1e-12;
+const ld PI = acos(-1);
+const int ON = 0, LEFT = 1, RIGHT = -1, BACK = -2, FRONT = 2, IN = 3, OUT = -3;
+
+inline bool Lss(ld a, ld b){ return a - b < -EPS; }
+inline bool Grt(ld a, ld b){ return a - b > +EPS; }	
+inline bool Leq(ld a, ld b){ return a - b < +EPS; }
+inline bool Geq(ld a, ld b){ return a - b > -EPS; }
+inline bool Equ(ld a, ld b){ return abs(a-b) < EPS; }
+
+bool byX(const pt &a, const pt &b)
+{
+	if (Equ(a.x, b.x)) return Lss(a.y, b.y);
+	return Lss(a.x, b.x);
+}
+bool byY(const pt &a, const pt &b){
+	if (Equ(a.y, b.y)) return Lss(a.x, b.x);
+	return Lss(a.y, b.y);
+}
+struct cmpXY{ inline bool operator ()(const pt &a, const pt &b)const { return byX(a, b); } };
+struct cmpYX{ inline bool operator ()(const pt &a, const pt &b)const { return byY(a, b); } };
+bool operator < (const pt &a, const pt &b){ return byX(a, b); }
+
+istream& operator >> (istream& in, pt p){ld valx,valy; in>>valx>>valy; p={valx,valy}; return in;}
+ostream& operator << (ostream& out, pt p){out<<p.x<<' '<<p.y; return out;}
+
+//Start:
+pt bary(pt A, pt B, pt C, double a, double b, double c) {
+    return (A*a + B*b + C*c) / (a + b + c);
+}
+pt centroid(pt A, pt B, pt C) {
+    // geometric center of mass
+    return bary(A, B, C, 1, 1, 1);
+}
+pt circumcenter(pt A, pt B, pt C) {
+    // intersection of perpendicular bisectors
+    double a = norm(B - C), b = norm(C - A), c = norm(A - B);
+    return bary(A, B, C, a*(b+c-a), b*(c+a-b), c*(a+b-c));
+}
+
+pt incenter(pt A, pt B, pt C) {
+    // intersection of internal angle bisectors
+    return bary(A, B, C, abs(B-C), abs(A-C), abs(A-B));
+}
+
+pt orthocenter(pt A, pt B, pt C) {
+    // intersection of altitudes
+    double a = norm(B - C), b = norm(C - A), c = norm(A - B);
+    return bary(A, B, C, (a+b-c)*(c+a-b), (b+c-a)*(a+b-c), (c+a-b)*(b+c-a));
+}
+
+pt excenter(pt A, pt B, pt C) {
+    // intersection of two external angle bisectors
+    double a = abs(B - C), b = abs(A - C), c = abs(A - B);
+    return bary(A, B, C, -a, b, c);
+
+    //// NOTE: there are three excenters
+    // return bary(A, B, C, a, -b, c);
+    // return bary(A, B, C, a, b, -c);
+}
+//END
+
+int main()
+{
+	ios_base::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+
+	return 0;
+}
